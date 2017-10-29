3e76943d423fae2c1e6029e444a0ee57e07dfabd
Catalin <icatalin201@gmail.com>
2017-10-29 04:20:27 +0200

    Fixed few things with fragments

---
+++ app/src/main/java/app/mov/movieteca/Home.java
@@ -0,0 +1,19 @@
+package app.mov.movieteca;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Catalin on 10/29/2017.
+ */
+
+public class Home extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ app/src/main/java/app/mov/movieteca/MainActivity.java
@@ -0,0 +1,70 @@
+package app.mov.movieteca;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle actionbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawer = (DrawerLayout)findViewById(R.id.drawer);
+        actionbar = new ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close);
+        drawer.addDrawerListener(actionbar);
+        actionbar.syncState();
+
+        // first launch with home fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.FragmentContainer, new Home()).commit();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(actionbar.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.nav1:
+                fragment = new Home();
+                break;
+            case R.id.nav2:
+                fragment = new About();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.FragmentContainer, fragment).commit();
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}

95250e1f21c2af4b430bf10bca010a85e6405c99
Catalin <icatalin201@gmail.com>
2018-02-27 17:11:52 +0200

    Add Splash Screen

---
+++ app/src/main/java/app/mov/movieteca/activities/Splash.java
@@ -0,0 +1,33 @@
+package app.mov.movieteca.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import app.mov.movieteca.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        Log.i("back", "pressed-haha");
+    }
+}
